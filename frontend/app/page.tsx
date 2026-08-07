3ab9d52e6308ab21b03837c5d68d63ba1de605f7
Syrine Chehairi <syrine.chehairi@hotmail.com>
2026-08-08 01:47:43 +0200

    feat: add vehicle creation form

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,5 @@
+import VehicleForm from "./components/VehicleForm";
+
 type Vehicle = {
   id: number;
   brand: string;
@@ -53,9 +55,7 @@ export default async function Home() {
               </p>
             </div>
 
-            <button className="rounded-lg bg-white px-4 py-2 font-medium text-black transition hover:bg-zinc-200">
-              + Ajouter un véhicule
-            </button>
+            <VehicleForm />
           </div>
 
           <div className="grid gap-5 md:grid-cols-2">
